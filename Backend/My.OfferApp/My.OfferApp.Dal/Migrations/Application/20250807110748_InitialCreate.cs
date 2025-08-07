1f0e34e4268d4b729a9c6e602954d9febee8a90b
varakuta <varakuta@ferratek.ru>
2025-08-07 14:30:20 +0300

    Создание проекта и добавление сущностей (Dal)

---
+++ Backend/My.OfferApp/My.OfferApp.Dal/Migrations/Application/20250807110748_InitialCreate.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace My.OfferApp.Dal.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "common");
+
+            migrationBuilder.EnsureSchema(
+                name: "dict");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "OfferId",
+                schema: "common");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "SupplierId",
+                schema: "dict");
+
+            migrationBuilder.CreateTable(
+                name: "Supplier",
+                schema: "dict",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR dict.SupplierId"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supplier", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Offer",
+                schema: "common",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR common.OfferId"),
+                    Mark = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SupplierId = table.Column<int>(type: "int", nullable: false),
+                    RegistrationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Offer_Supplier_SupplierId",
+                        column: x => x.SupplierId,
+                        principalSchema: "dict",
+                        principalTable: "Supplier",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offer_SupplierId",
+                schema: "common",
+                table: "Offer",
+                column: "SupplierId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Offer",
+                schema: "common");
+
+            migrationBuilder.DropTable(
+                name: "Supplier",
+                schema: "dict");
+
+            migrationBuilder.DropSequence(
+                name: "OfferId",
+                schema: "common");
+
+            migrationBuilder.DropSequence(
+                name: "SupplierId",
+                schema: "dict");
+        }
+    }
+}
---
+++ Backend/My.OfferApp/My.OfferApp.Dal/Migrations/Application/20250807110838_AddFakeData.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace My.OfferApp.Dal.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class AddFakeData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+INSERT INTO
+  [dict].[Supplier] ([Name], [CreatedDate])
+VALUES
+  (
+    'ООО Поставка 1',
+    '2025-06-11T12:09:47.1740431'
+  ),
+  (
+    'АО Снабжение 2',
+    '2025-07-08T12:09:47.1740431'
+  ),
+  ('ИП Иванов 3', '2025-07-11T12:09:47.1740431'),
+  (
+    'ЗАО Логистика 4',
+    '2025-08-01T12:09:47.1740431'
+  ),
+  ('Поставщик 5', '2025-08-06T12:09:47.1740431');
+"
+            );
+
+            migrationBuilder.Sql(
+                @"
+INSERT INTO
+  [common].[Offer] (
+    [Mark],
+    [Model],
+    [SupplierId],
+    [RegistrationDate]
+  )
+VALUES
+  (
+    'Toyota',
+    'Camry',
+    1,
+    '2025-06-01T12:09:47.1740431'
+  ),
+  (
+    'Hyundai',
+    'Solaris',
+    2,
+    '2025-06-12T12:09:47.1740431'
+  ),
+  ('Kia', 'Rio', 3, '2025-07-08T12:09:47.1740431'),
+  (
+    'Volkswagen',
+    'Polo',
+    4,
+    '2025-07-09T12:09:47.1740431'
+  ),
+  (
+    'Renault',
+    'Logan',
+    5,
+    '2025-07-15T12:09:47.1740431'
+  ),
+  ('Ford', 'Focus', 1, '2025-07-15T16:09:47.1740431'),
+  (
+    'Chevrolet',
+    'Cruze',
+    2,
+    '2025-07-22T12:09:47.1740431'
+  ),
+  (
+    'Skoda',
+    'Octavia',
+    3,
+    '2025-07-25T12:09:47.1740431'
+  ),
+  (
+    'Nissan',
+    'Almera',
+    4,
+    '2025-07-25T13:09:47.1740431'
+  ),
+  ('Mazda', '3', 5, '2025-07-25T18:09:47.1740431'),
+  (
+    'Lada',
+    'Vesta',
+    1,
+    '2025-08-01T12:09:47.1740431'
+  ),
+  (
+    'Lada',
+    'Granta',
+    2,
+    '2025-08-03T10:09:47.1740431'
+  ),
+  (
+    'Geely',
+    'Coolray',
+    3,
+    '2025-08-03T12:09:47.1740431'
+  ),
+  (
+    'Chery',
+    'Tiggo 4',
+    4,
+    '2025-08-06T12:09:47.1740431'
+  ),
+  (
+    'Haval',
+    'Jolion',
+    5,
+    '2025-08-07T15:09:47.1740431'
+  );
+"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [common].[Offer]");
+
+            migrationBuilder.Sql("DELETE FROM [dict].[Supplier]");
+        }
+    }
+}
